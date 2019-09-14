5a34343f95cd01438e06d3bf4a1aef8d789480bf
Amran Ahmed <amranahmedd@gmail.com>
2019-09-14 12:20:02 -0500

    made a button and imported bootstrap

--- Angular-Project/src/app/app.component.ts
+++ Angular-Project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Type } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Project';
+  num = 0; //variable: type;
+
+ nameNamer (){ 
+   let name: any = 'Ahmed'; //any allows the variable name to be dynamic
+   name = 6;
+ }
+
+ onInc() {
+   console.log('this Inc button was clicked on!');
+   this.num++;
+ }
+  
 }
